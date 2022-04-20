68c3d6775df04686587eae4ec8a8f505e8b502b4
phoebe <cheezepeperoni@gmail.com>
2022-04-20 15:38:15 +0900

    On Wednesday 0420

---
+++ react-masterclass/src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle"
+
+function App() {
+  return (
+   <div>
+     <Circle borderColor="purple" bgColor="lavender"/>
+     <Circle bgColor="pink"/>
+   </div>
+  );
+}
+
+export default App;
