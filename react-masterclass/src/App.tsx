7499f9a2dc56ba1d373f5cad1551c6b79066b5eb
phoebe <cheezepeperoni@gmail.com>
2022-04-20 17:40:40 +0900

    On wednesday 0420

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,11 +1,23 @@
-import Circle from "./Circle"
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+  width:100%;
+  height:100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const H1 = styled.h1`
+  color:${(props)=>props.theme.textColor}
+`;
 
 function App() {
   return (
-   <div>
-     <Circle borderColor="purple" bgColor="lavender"/>
-     <Circle bgColor="pink"/>
-   </div>
+   <Container>
+    <H1>HI!</H1>
+   </Container>
   );
 }
 
