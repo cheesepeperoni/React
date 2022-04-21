4bf8a028d23b20ff91f6486b0d14c39ae74fc0a2
phoebe <cheezepeperoni@gmail.com>
2022-04-21 16:32:57 +0900

    On Thursday 0421

--- react-masterclass/src/routes/Coin.tsx
+++ react-masterclass/src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation, useParams } from 'react-router-dom'
 import styled from 'styled-components';
 
@@ -9,6 +9,62 @@ coinId:string;
 interface RouteState{
   name:string;
 }
+
+interface InfoData{
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+interface PriceData{
+  id:string;
+  name:string;
+  symbol:string;
+  rank:number;
+  circulating_supply:number;
+  total_supply:number;
+  max_supply:number;
+  beta_value:number;
+  first_data_at:string;
+  last_updated:string;
+  quotes:{
+    USD:{
+      ath_date: string;
+      ath_price:number;
+      market_cap:number;
+      market_cap_change_24h:number;
+      percent_change_1h:number;
+      percent_change_1y:number;
+      percent_change_6h:number;
+      percent_change_7d:number;
+      percent_change_12h:number;
+      percent_change_15m:number;
+      percent_change_24h:number;
+      percent_change_30d:number;
+      percent_change_30m:number;
+      percent_from_price_ath:number;
+      price:number;
+      volume_24h:number;
+      volume_24h_change_24h:number;
+    }
+  };
+}
+
+
 const Container = styled.div`
   padding:0px 20px;
   max-width: 480px;
@@ -31,18 +87,85 @@ const Loader = styled.span`
   text-align: center;
   display: block;
 `;
+const Overview= styled.div`
+  display: flex;
+  justify-content: space-between;
+  border-radius: 10px;
+  background-color:rgba(0, 0, 0, 0.5);
+  padding:10px 20px;
+`;
 
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span:first-child{
+    font-size:10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom:5px;
+  }
+`;
+
+const Description = styled.p`
+  margin:20px 0px;
+
+`;
 const Coin = () => {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<coinIdParams>();
   const { state } = useLocation<RouteState>();
-console.log(state.name)
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+  useEffect(()=>{
+    (async ()=>{
+      const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+      const priceData = await(await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      setLoading(false);
+    })()
+  },[coinId])
   return (
   <Container>
     <Header>
-      <Title>{state?.name || "Loading"}</Title>
+      <Title>{state?.name ?state.name : loading? "Loading":info?.name}</Title>
     </Header>
-    {loading ? <Loader>"...Loading"</Loader>:null}
+    {loading ? <Loader>"...Loading"</Loader>:
+      <>
+      <Overview>
+        <OverviewItem>
+          <span>Rank</span>
+          <span>{info?.rank}</span>
+        </OverviewItem>
+        <OverviewItem>
+          <span>symbol</span>
+          <span>${info?.symbol}</span>
+        </OverviewItem>
+        <OverviewItem>
+          <span>Open Source</span>
+          <span>{info?.open_source? "Yes":"NO"}</span>
+        </OverviewItem>
+      </Overview>
+           
+      <Description>
+        <span>{info?.description}</span>
+      </Description> 
+
+      <Overview>
+        <OverviewItem>
+          <span>Total Suply</span>
+          <span>{priceInfo?.total_supply}</span>
+        </OverviewItem>
+        <OverviewItem>
+          <span>Max Suply</span>
+          <span>{priceInfo?.max_supply}</span>
+        </OverviewItem>
+      </Overview>
+      </>
+    }
   </Container>
   )}
 
